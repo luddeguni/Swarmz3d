21805df9664c6830945bd8cc922ea9b32de8d3c4
Eddan05 <ht24edfr@axevalla.fhsk.se>
2025-03-24 13:50:35 +0100

    0.7

--- Assets/Player Related/waveSpawner.cs
+++ Assets/Player Related/waveSpawner.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerPowerups : MonoBehaviour
+public class waveSpawner : MonoBehaviour
 {
-    float[] powerUp = { 1f, 0.3f, 0.5f, 0.7f, 0.9f };
     // Start is called before the first frame update
     void Start()
     {
