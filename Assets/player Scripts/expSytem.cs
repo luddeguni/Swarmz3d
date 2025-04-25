270c5b559eb8a881c5115fb3d16d065ac8e4840d
Eddan05 <ht24edfr@axevalla.fhsk.se>
2025-04-25 08:41:49 +0200

    lalala

--- Assets/player Scripts/expSytem.cs
+++ Assets/player Scripts/expSytem.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class expSytem : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int level = 1; // Current player level
+    public int exp = 0; // Current experience points
+    public int expRequired = 100; // EXP required to level up
+
+    // Method to gain experience
+    public void GainExp(int amount)
     {
-        
+        exp += amount;
+        Debug.Log($"Player gained {amount} EXP. Total EXP: {exp}");
+
+        // Check if the player has enough EXP to level up
+        while (exp >= expRequired)
+        {
+            LevelUp();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Method to level up
+    void LevelUp()
     {
-        
+        level++; // Increase level
+        exp -= expRequired; // Deduct the EXP required for the level up
+        expRequired *= 2; // Double the EXP required for the next level up
+        Debug.Log($"Level up! Player is now level {level}. Next level requires {expRequired} EXP.");
+    }
+
+    // For testing purposes, we can call this method when the game starts or during an event
+    private void Start()
+    {
+        // For example, gain 250 EXP to see a few level-ups
+        GainExp(250);
     }
 }
